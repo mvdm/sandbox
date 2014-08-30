c553043b68780a783aa322a57b44ec61531a5d85
ecarmichael <ecarmichael@gmail.com>
2014-08-29 21:24:47 -0400

    Reverted the piecewise to Eric's org version
    This version works.  Please ask me before you change anything.  Other
    versions were not yielding the right answers since they were not giving
    0 outputs fro channels 2,4

--- Gurney2001/piecewise.m
+++ Gurney2001/piecewise.m
@@ -1,26 +1,24 @@
 function [y] = piecewise(a, epsilon, m)
-% takes a matrix of size n a as the activation values
-
-output(a<epsilon) = 0; 
-output(a>=epsilon & a<= 1/m +epsilon) = m*(a(a>=epsilon & a<= 1/m +epsilon)-epsilon);
-output(a>(1/m)+epsilon) = 1;   
-y = a;
-
+% piecewise
+%
+% Takes a matrix of size n a as the activation values
 % a(a<epsilon) = 0;
 % a((epsilon <= a) & (a <= (1/m+epsilon))) = m*(a-epsilon);
 % a(a>(1/m+epsilon)) = 1;
-
-% for i = 1:length(a)
-%     if a(i) < epsilon;
-%         a(i) = 0;
-%     elseif ((epsilon <= a(i)) && (a(i) <= (1/m+epsilon)));
-%         a(i) = m*(a(i)-epsilon);
-%     else a(i) > 1/m+epsilon;
-%         a(i) = 1;
-%     end
-% end
-% 
 % y = a;
-% 
-% end
-% 
+
+for i = 1:length(a)
+    if a(i) < epsilon;
+        a(i) = 0;
+    elseif ((epsilon <= a(i)) && (a(i) <= (1/m+epsilon)));
+        a(i) = m*(a(i)-epsilon);
+    else a(i) > 1/m+epsilon;
+        a(i) = 1;
+    end
+
+end
+
+y = a;  % corresponds to the output of the nodes
+
+end
+
